75c0194af66ec32cf5b9228af74d33209ab12771
Riqki <RiqkiAfandi789@gmail.com>
2014-12-07 00:38:34 +0700

    Versi 1.0

---
+++ Project/WTCBatangHari/src/com/Riqki/wtcbatanghari/BakmiActivity.java
@@ -0,0 +1,22 @@
+package com.Riqki.wtcbatanghari;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class BakmiActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_bakmi);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.bakmi, menu);
+		return true;
+	}
+
+}
